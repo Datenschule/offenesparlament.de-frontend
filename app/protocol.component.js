524f63cf28be2646e9e9afb8337778e70a1078c3
Knut Perseke <knut.perseke@gmail.com>
2017-08-13 18:30:53 +0200

    add base url also to protocol.component

--- app/protocol.component.js
+++ app/protocol.component.js
@@ -1,10 +1,12 @@
+const BASE_URL = API_BASE_URL;
+
 const protocol = {
     template: require("./protocol.html"),
     controller: function ($http, $location) {
         this.utterances = [];
         this.$onInit = () => {
             const linkTarget = $location.hash();
-            $http.get(`http://localhost:5000/api/session${$location.path()}`).then(
+            $http.get(`${BASE_URL}/api/session${$location.path()}`).then(
                 (resp) => {
                     this.utterances = resp.data.data.reduce((acc, currentValue, currentIndex, array) => {
                       if (currentIndex > 1 && currentValue.top !== array[currentIndex -1].top) {
