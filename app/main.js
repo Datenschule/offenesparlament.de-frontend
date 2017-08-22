5af112e7b16eb32d68ea002a06b335185d07657f
Knut Perseke <knut.perseke@gmail.com>
2017-08-22 11:07:17 +0200

    add new components to angular app and update assets

--- app/main.js
+++ app/main.js
@@ -1,18 +1,27 @@
 import * as angular from "angular";
 import ngAnimate from "angular-animate"
+import angularChartist from "angular-chartist.js"
+
+// import chartistBarLabels from "./lib/chartist-bar-labels"
+
+
 
 import protocolOverviewComponent from "./protocol-overview.component";
 import poiComponent from "./poi.component"
 import protocolComponent from "./protocol.component"
 import speechComponent from "./speech.component"
 import top from "./top.component"
+import subjectVizComponent from "./subject-viz.component"
+import speechFrequencyComponent from "./speech-frequency.component-viz"
 
-angular.module('offenesparlament', [ngAnimate])
+angular.module('offenesparlament', [ngAnimate, angularChartist])
     .component('protocols', protocolOverviewComponent)
     .component('protocol', protocolComponent)
     .component('speech', speechComponent)
     .component('poi', poiComponent)
     .component('top', top)
+    .component('subjectViz', subjectVizComponent)
+    .component('speechFrequency', speechFrequencyComponent)
     .filter('newlines', function () {
         return function(text) {
             return text.replace(/\n/g, '<br/>');
