3eb2aac7034d6f2731f06f8c48c1d4def9848937
Knut Perseke <knut.perseke@gmail.com>
2017-08-25 18:19:35 +0200

    add basic protocol navigation

--- app/main.js
+++ app/main.js
@@ -3,6 +3,9 @@ import ngAnimate from "angular-animate"
 import ui_select from "ui-select"
 import angularChartist from "angular-chartist.js"
 import ngSanitize from "angular-sanitize"
+import duScroll from "angular-scroll"
+import angularInview from 'angular-inview';
+// import smoothScroll from 'ngSmoothScroll';
 
 // import chartistBarLabels from "./lib/chartist-bar-labels"
 
@@ -16,7 +19,7 @@ import top from "./top.component"
 import subjectVizComponent from "./subject-viz.component"
 import speechFrequencyComponent from "./speech-frequency.component-viz"
 
-angular.module('offenesparlament', [ngAnimate, angularChartist, ui_select, ngSanitize])
+let app = angular.module('offenesparlament', [ngAnimate, angularChartist, ui_select, ngSanitize, duScroll, angularInview.name])
     .component('protocols', protocolOverviewComponent)
     .component('protocol', protocolComponent)
     .component('speech', speechComponent)
@@ -24,8 +27,47 @@ angular.module('offenesparlament', [ngAnimate, angularChartist, ui_select, ngSan
     .component('top', top)
     .component('subjectViz', subjectVizComponent)
     .component('speechFrequency', speechFrequencyComponent)
-    .filter('newlines', function () {
+	.value('duScrollOffset', -200);
+
+app.filter('newlines', function () {
         return function(text) {
             return text.replace(/\n/g, '<br/>');
         }
+    });
+
+app.directive("stickyNav", function stickyNav($window){
+        function stickyNavLink(scope, element){
+            var w = angular.element($window),
+                size = element[0].clientHeight,
+                top = 0;
+
+            function toggleStickyNav(){
+                if(!element.hasClass('controls-fixed') && $window.pageYOffset > top + size){
+                    element.addClass('controls-fixed');
+                } else if(element.hasClass('controls-fixed') && $window.pageYOffset <= top + size){
+                    element.removeClass('controls-fixed');
+                }
+            }
+
+            scope.$watch(function(){
+                return element[0].getBoundingClientRect().top + $window.pageYOffset;
+            }, function(newValue, oldValue){
+                if(newValue !== oldValue && !element.hasClass('controls-fixed')){
+                    top = newValue;
+                }
+            });
+
+            w.bind('resize', function stickyNavResize(){
+                element.removeClass('controls-fixed');
+                top = element[0].getBoundingClientRect().top + $window.pageYOffset;
+                toggleStickyNav();
+            });
+            w.bind('scroll', toggleStickyNav);
+        }
+
+        return {
+            scope: {},
+            restrict: 'A',
+            link: stickyNavLink
+        };
     });
--- app/protocol.component.js
+++ app/protocol.component.js
@@ -1,16 +1,21 @@
+import { kebabCase } from "lodash";
+
 const BASE_URL = API_BASE_URL;
 
 const protocol = {
     template: require("./protocol.html"),
-    controller: function ($http, $location) {
+    controller: function ($http, $location, $anchorScroll) {
         this.utterances = [];
         this.$onInit = () => {
             const linkTarget = $location.hash();
             $http.get(`${BASE_URL}/api/session${$location.path()}`).then(
                 (resp) => {
+                    this.tops = [];
+                    this.selectedTop = {};
                     this.utterances = resp.data.data.reduce((acc, currentValue, currentIndex, array) => {
                       if (currentIndex > 1 && currentValue.top !== array[currentIndex -1].top) {
                           acc.push({type: 'top', 'title': currentValue.top})
+                          this.tops.push({type: 'top', 'title': currentValue.top, 'link': kebabCase(currentValue.top)})
                       }
                       if (currentValue.sequence === parseInt(linkTarget, 10)) {
                           currentValue.linked = true;
@@ -18,8 +23,21 @@ const protocol = {
                       acc.push(currentValue);
                       return acc;
                     }, []);
-                }
+					this.session = this.utterances[0]['sitzung'];
+                    this.date = "15.03.2017";
+                    this.current_top = "TOP 40 Bundeswehreinsatz im Mittelmeer (SOPHIA)"
+
+				}
             );
+        }
+        this.selectTop = function(link) {
+            console.log('scrolled by' + link);
+			// var element = document.getElementById(link);
+			// angular.element(document.body).scrollToElement(element);
+			$location.hash(link);
+			$anchorScroll.yOffset = 200;
+			// call $anchorScroll()
+			$anchorScroll();
         }
     }
 }
