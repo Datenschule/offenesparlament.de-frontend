a27b72bfa9a6652222c4e2dfdf0d6999c5182f65
Knut Perseke <knut.perseke@gmail.com>
2017-08-31 11:40:17 +0200

    add embedding

--- static/js/refill.js
+++ static/js/refill.js
@@ -79,4 +79,22 @@ $(document).ready(function() {
 			$('.dropdown-button').append('<span class="dropdown-button-arrow pull-right"><i class="fa fa-chevron-down"></i></span>');
 		});
 	});
+
+	$(function() {
+		$("#modal-1").on("change", function() {
+			if ($(this).is(":checked")) {
+				$("body").addClass("modal-open");
+			} else {
+				$("body").removeClass("modal-open");
+			}
+		});
+
+		$(".modal-fade-screen, .modal-close").on("click", function() {
+			$(".modal-state:checked").prop("checked", false).change();
+		});
+
+		$(".modal-inner").on("click", function(e) {
+			e.stopPropagation();
+		});
+	});
 });
