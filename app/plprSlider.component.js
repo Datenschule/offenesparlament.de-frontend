276eb57281480a80cefe28645c0acadd1824042c
Knut Perseke <knut.perseke@gmail.com>
2017-09-04 17:36:43 +0200

    update slider

--- app/plprSlider.component.js
+++ app/plprSlider.component.js
@@ -2,13 +2,18 @@ import {includes} from "lodash";
 
 const slider = {
 	template: `
-     <ui-carousel slides="$ctrl.single.slides" dots="true" arrows="true" speed="800" style="width:50%; margin:auto;">
+     <ui-carousel slides="$ctrl.single.slides" dots="true" arrows="true" speed="800" style="">
      	<carousel-item>
-     		<div ng-class="item.style">
-    			<p><strong>{{item.title}}</strong></p>
-    			<p>{{item.text}}</p>
-    			<p>({{item.speaker}})</p>
+			<div ng-class="item.style" class="carousel">
+				<div class="carousel-overlay">  
+					<div class="carousel-container">
+						<p><strong>{{item.title}}</strong></p>
+						<p>{{item.text}}</p>
+						<p>({{item.speaker}})</p>		  			
+					</div>			
+				</div>
     		</div>
+    			
   		</carousel-item>
 		
   		
@@ -31,28 +36,28 @@ const slider = {
 			this.single = {};
 			this.single.slides = [
 				{
-					style: 'carousel-1 carousel',
+					style: 'carousel-1',
 					title: "Plattdeutsch im Bundestag",
 					text: "„Spraak is Heimat, Spraak hett wat mit Identität to doon, wi weet, wo wi herkaamt, un wi köönt kieken, wo wi henwüllt.“",
 					speaker: "Herbert Behrens Die LINKE"
 
 				},
 				{
-					style: 'carousel-2  carousel',
+					style: 'carousel-2',
 					title: "Zu Deutschlands Politik in der Debatte zur Bankenunion",
 					text: "drei kleine Schweinchen„Die Politik, die Sie in Brüssel betreiben, erinnert mich an die Die drei kleinen Schweinchen.“",
 					speaker: "Manuel Sarrazin, Bündnis '90 die Grünen"
 
 				},
 				{
-					style: 'carousel-3  carousel',
+					style: 'carousel-3',
 					title: "Plattdeutsch im Bundestag",
 					text: "„Spraak is Heimat, Spraak hett wat mit Identität to doon, wi weet, wo wi herkaamt, un wi köönt kieken, wo wi henwüllt.“",
 					speaker: "Herbert Behrens Die LINKE"
 
 				},
 				{
-					style: 'carousel-4  carousel',
+					style: 'carousel-4',
 					title: "In der Debatte zur Ehe für Alle",
 					text: "„Vor lauter Schreck ist Horst Seehofer dann aber leider krank geworden.“",
 					speaker: "(Caren Lay, DIE LINKE)"
