7da782f6f39e544dc4205f9819f1a408a8b2dc54
Knut Perseke <knut.perseke@gmail.com>
2017-09-28 16:30:16 +0200

    update cites for landing page

--- app/plprSlider.component.js
+++ app/plprSlider.component.js
@@ -2,13 +2,13 @@ import {includes} from "lodash";
 
 const slider = {
 	template: `
-     <ui-carousel slides="$ctrl.single.slides" dots="true" arrows="true" speed="800" style="">
+     <ui-carousel slides="$ctrl.single.slides" dots="true" arrows="true" speed="800" style="" autoplay="true" autoplay-speed="8000">
      	<carousel-item>
 			<div ng-class="item.style" class="carousel">
 				<div class="carousel-overlay">  
 					<div class="carousel-container">
 						<p><strong>{{item.title}}</strong></p>
-						<p>{{item.text}}</p>
+						<p ng-bind-html="item.text"></p>
 						<p>({{item.speaker}})</p>		  			
 					</div>			
 				</div>
@@ -37,30 +37,24 @@ const slider = {
 			this.single.slides = [
 				{
 					style: 'carousel-1',
-					title: "Zur Förderung von Minderheitensprachen",
-					text: "„Spraak is Heimat, Spraak hett wat mit Identität to doon, wi weet, wo wi herkaamt, un wi köönt kieken, wo wi henwüllt.“",
-					speaker: "Herbert Behrens, Die LINKE"
-
-				},
-				{
-					style: 'carousel-2',
-					title: "Zu Deutschlands Politik in der Debatte zur Bankenunion",
-					text: "„Die Politik, die Sie in Brüssel betreiben, erinnert mich an die Die drei kleinen Schweinchen.“",
-					speaker: "Manuel Sarrazin, Bündnis '90 die Grünen"
+					title: "Debatte zur Ehe für Alle",
+					text: "„Meine Kolleginnen und Kollegen von der Union, wer will denn in Deutschland die Ehe für alles? Für alle! - 83 Prozent aller Deutschen wollen sie.“",
+					speaker: "Karl-Heinz Brunner, SPD"
 
 				},
 				{
 					style: 'carousel-3',
 					title: "Plattdeutsch im Bundestag",
-					text: "„Spraak is Heimat, Spraak hett wat mit Identität to doon, wi weet, wo wi herkaamt, un wi köönt kieken, wo wi henwüllt.“",
-					speaker: "Herbert Behrens, Die LINKE"
+					text: "„Die Zusammenarbeit zwischen unserer Untersuchungskommission und Volkswagen funktioniert übrigens kooperativ.”</p><p>(Alexander Dobrindt, CSU)</p><p><i>“Das glaube ich ausnahmsweise!”</i></p>",
+					speaker: 'Anton Hofreiter, BÜNDNIS 90/DIE GRÜNEN'
+
 
 				},
 				{
 					style: 'carousel-4',
-					title: "In der Debatte zur Ehe für Alle",
-					text: "„Vor lauter Schreck ist Horst Seehofer dann aber leider krank geworden.“",
-					speaker: "Caren Lay, DIE LINKE"
+					title: "Debatte zur Aufnahme von Geflüchteten",
+					text: "“Ja, wir brauchen auch - auch das will ich sagen; das habe ich noch nie in meinem Leben gesagt - die vielen Mitglieder der CDU, die den Ausspruch von Angela Merkel „Wir schaffen das“ als einen Auftrag zur Nächstenliebe im Alltag begriffen haben”",
+					speaker: "Jan Korte, DIE LINKE"
 				}
 			]
 		}
